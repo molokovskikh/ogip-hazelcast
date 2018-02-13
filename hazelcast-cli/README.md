f68ea701065b99686b74652be6f79535bda4df77
Sergey Molokovskih <s.molokovskih@innopolis.ru>
2018-02-13 11:15:39 +0300

    Added .gitignore

--- hazelcast-cli/README.md
+++
@@ -1,9 +0,0 @@
-# hcs-dev-hazelcast
-# Hazelcast для hcs-dev
-* git clone git@github.com:molokovskikh/hcs-dev-hazelcast.git
-* cd hcs-dev-hazelcast
-* gradle installDist
-* cd build/install/hazelcast/bin/
-* Run hcs-dev-hazelcast or hcs-dev-hazelcast.cmd (For Windows)
-* You can placed config according to documentation also.
-* https://github.com/hazelcast/hazelcast-gradle-starter/blob/master/README.adoc
--- symlink.sh
+++ symlink.sh
@@ -5,7 +5,7 @@ cur_dir=$(pwd)
 cd hazelcast && ./gradlew installDist && ln -fs $(pwd)/build/install/hazelcast/bin/hazelcast /usr/bin/hazelcast
 cd $cur_dir
 
-cd hazelcast-cli && ./gradlew installDist && ln -fs $(pwd)/build/install/hazelcast/bin/hazelcast /usr/bin/hazelcast-cli
+cd hazelcast-cli && ./gradlew installDist && ln -fs $(pwd)/build/install/hazelcast-cli/bin/hazelcast-cli /usr/bin/hazelcast-cli
 cd $cur_dir
 
 
