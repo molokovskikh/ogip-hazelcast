812258ff21df764058c539aabed1698724249542
Сергей Молоковских <s.molokovskih@innopolis.ru>
2018-02-13 08:37:37 +0000

    Updated symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -8,4 +8,5 @@ cd $cur_dir
 cd hazelcast-cli && ./gradlew installDist && ln -fs $(pwd)/build/install/hazelcast-cli/bin/hazelcast-cli /usr/bin/hazelcast-cli
 cd $cur_dir
 
-
+ln -fs $(pwd)/hazelcast/src/main/resources/hazelcast.xml hazelcast.xml
+ln -fs $(pwd)/hazelcast-cli/src/main/resources/hazelcast-client.xml hazelcast-client.xml
