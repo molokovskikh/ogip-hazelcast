9ea18f0119a14b4136178215b4fc056885e881ad
Sergey Molokovskih <s.molokovskih@innopolis.ru>
2018-02-13 10:57:20 +0300

    Init repository

---
+++ symlink.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cur_dir=$(pwd)
+
+cd hazelcast && ./gradlew installDist && ln -fs $(pwd)/build/install/hazelcast/bin/hazelcast /usr/bin/hazelcast
+cd $cur_dir
+
+cd hazelcast-cli && ./gradlew installDist && ln -fs $(pwd)/build/install/hazelcast/bin/hazelcast /usr/bin/hazelcast-cli
+cd $cur_dir
+
+
